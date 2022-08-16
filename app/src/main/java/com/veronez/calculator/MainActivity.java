3347c70006886db1b89a3036335fb89e4ab78e13
Daniel Veronez <danielveronezb@gmail.com>
2022-08-15 23:53:28 -0300

    Atualização - Adicionado função para pegar o retorno da tecla selecionada

--- app/src/main/java/com/veronez/calculator/MainActivity.java
+++ app/src/main/java/com/veronez/calculator/MainActivity.java
@@ -3,11 +3,12 @@ package com.veronez.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis, numeroSete,
             numeroOito, numeroNove, ponto, soma, subritacao, multiplicacao, divisao, igual, botao_Limpar;
@@ -23,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        numeroZero.setOnClickListener(this);
+        numeroUm.setOnClickListener(this);
+        numeroDois.setOnClickListener(this);
+        numeroTres.setOnClickListener(this);
+        numeroQuatro.setOnClickListener(this);
+        numeroCinco.setOnClickListener(this);
+        numeroSeis.setOnClickListener(this);
+        numeroSete.setOnClickListener(this);
+        numeroOito.setOnClickListener(this);
+        numeroNove.setOnClickListener(this);
+        ponto.setOnClickListener(this);
+        soma.setOnClickListener(this);
+        subritacao.setOnClickListener(this);
+        multiplicacao.setOnClickListener(this);
+        divisao.setOnClickListener(this);
+
+        botao_Limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtExprecao.setText("");
+                txtResultado.setText("");
+            }
+        });
     }
 
     private void iniciarComponentes(){
@@ -51,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void acrescentarUmaExprecao(String string, Boolean limpar_dados){
+    public void AcrescentarUmaExprecao(String string, Boolean limpar_dados){
 
         if (txtResultado.getText().equals("")){
             txtExprecao.setText("");
@@ -63,7 +88,70 @@ public class MainActivity extends AppCompatActivity {
         }else{
             txtExprecao.append(txtResultado.getText());
             txtExprecao.append(string);
+            txtResultado.setText("");
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.numero_zero:
+                AcrescentarUmaExprecao("0",true);
+                break;
+
+            case R.id.numero_um:
+                AcrescentarUmaExprecao("1",true);
+                break;
+
+            case R.id.numero_tres:
+                AcrescentarUmaExprecao("3", true);
+                break;
+
+            case R.id.numero_quatro:
+                AcrescentarUmaExprecao("4", true);
+                break;
+
+            case R.id.numero_cinco:
+                AcrescentarUmaExprecao("5", true);
+                break;
+
+            case R.id.numero_seis:
+                AcrescentarUmaExprecao("6", true);
+                break;
+
+            case R.id.numero_sete:
+                AcrescentarUmaExprecao("7", true);
+                break;
+
+            case R.id.numero_oito:
+                AcrescentarUmaExprecao("8", true);
+                break;
+
+            case R.id.numero_nove:
+                AcrescentarUmaExprecao("9", true);
+                break;
+
+            case R.id.ponto:
+                AcrescentarUmaExprecao(".", true);
+                break;
+
+            case R.id.soma:
+                AcrescentarUmaExprecao("+", false);
+                break;
+
+            case R.id.subitracao:
+                AcrescentarUmaExprecao("-", false);
+                break;
+
+            case R.id.multiplicacao:
+                AcrescentarUmaExprecao("*", false);
+                break;
+
+            case R.id.divisao:
+                AcrescentarUmaExprecao("/", false);
+                break;
+        }
     }
 }
